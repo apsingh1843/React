430cc5d44de60ef54e3b84162075dc8827899aac
Ayush Pratap Singh <>
2021-05-08 17:22:12 +0530

    redux thunk

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Label, Row, Col, Modal, ModalBody, ModalHeader } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors} from 'react-redux-form';
-
+import { Loading } from './LoadingComponent';
 
 
 function RenderDish({dish}) {
@@ -48,7 +48,25 @@ else {
 }
 
 const DishDetail= (props) => {
-  if (props.dish != null) {
+  if(props.isLoading) {
+    return(
+      <div className="container">
+        <div className="row">
+           <Loading />
+        </div>
+      </div>
+    );
+  }
+  else if(props.errMess) {
+   return(
+     <div className="container">
+       <div className="row">
+          <h4>{props.errMess}</h4>
+       </div>
+     </div>
+   );
+  }
+  else if (props.dish != null) {
     return(
       <div className="container">
         <div className="row">
--- confusion/src/redux/ActionCreators.js
+++ confusion/src/redux/ActionCreators.js
@@ -1,4 +1,5 @@
 import * as ActionTypes from './ActionTypes';
+import { DISHES } from '../shared/dishes';
 
 export const addComment = (dishId, rating, author, comment) => ({
   //returns plain JS object
@@ -9,4 +10,26 @@ export const addComment = (dishId, rating, author, comment) => ({
     author: author,
     comment: comment,
   }
+});
+
+//thunk (returns a function)
+export const fetchDishes = () => (dispatch) => {
+  dispatch(dishesLoading(true));
+  setTimeout(() => {
+    dispatch(addDishes(DISHES));
+  }, 3000);
+};
+
+export const dishesLoading = () =>({
+  type: ActionTypes.DISHES_LOADING
+});
+
+export const dishesFailed = (errmess) => ({
+  type: ActionTypes.DISHES_FAILED,
+  payload: errmess
+});
+
+export const addDishes = (dishes) => ({
+  type: ActionTypes.ADD_DISHES,
+  payload: dishes
 });
