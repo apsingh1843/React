6b2fce63bf28fd78c685d0c5533061525b235cde
Ayush Pratap Singh <>
2021-05-13 00:56:57 +0530

    contact form revisited

--- confusion/src/components/ContactComponent.js
+++ confusion/src/components/ContactComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Breadcrumb, BreadcrumbItem, Button, Label, Col, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Control, Form, Errors, actions } from 'react-redux-form';
 
 
 const required = (val) => val && val.length;
@@ -24,6 +24,7 @@ class Contact extends Component {
   handleSubmit(values) {
     console.log("Current State is: " + JSON.stringify(values));
     alert("Current State is: " + JSON.stringify(values));
+    this.props.resetFeedbackForm();
     //event.preventDefault();
   }
 
@@ -68,7 +69,7 @@ class Contact extends Component {
             <h3>Send Us Your Feedback</h3>
           </div>
           <div className="col-12 col-md-9">
-            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+            <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
               <Row className="form-group">
                   <Label htmlFor="firstname" md={2}>First Name</Label>
                   <Col md={10}>
@@ -129,7 +130,7 @@ class Contact extends Component {
                 <Col md={{size:6, offset:2}}>
                   <div className="form-check">
                     <Label check>
-                       <Control.checkbox model=".agree" type="checkbox" name="agree" className="Form-check-input"/> {' '}
+                       <Control.checkbox model=".agree" name="agree" className="form-check-input"/> {' '}
                        <strong>May we contact you?</strong>
                     </Label>
                   </div>
@@ -152,7 +153,7 @@ class Contact extends Component {
                    <Button type="submit" color="primary">Send Feedback</Button>
                 </Col>
               </Row>
-            </LocalForm>
+            </Form>
           </div>
         </div>
       </div>
